15c92add405e03b55e0ccd443ea6ae4683e832d8
Francisco Márquez <francisco.marquez.solt@gmail.com>
2021-08-22 18:23:03 -0500

    Challenge

--- challenges/index.js
+++ challenges/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get("/user/:id", function(req, res) {
-  res.send("user " + req.params.id);
+app.get('/', function (req, res) {
+  res.send(
+    'Please introduce the year in the url like this localhost:3000/2019'
+  );
+});
+
+app.get('/:year', (req, res) => {
+  const year = parseInt(req.params.year);
+  if ((year % 4 === 0 && year & (100 !== 0)) || year % 400 === 0) {
+    res.send('Is leap year');
+  } else {
+    res.send('Is not leap-year');
+  }
 });
 
 app.listen(port, function () {
